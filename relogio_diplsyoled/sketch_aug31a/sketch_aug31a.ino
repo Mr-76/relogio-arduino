6ba8c18773f3db8acb59561801c2b3eeb205f5b8
holand76 <thegreath76@gmail.com>
2021-10-15 14:27:05 -0300

    funçoes feita e display com data e hora

--- relogio_diplsyoled/sketch_aug31a/sketch_aug31a.ino
+++ relogio_diplsyoled/sketch_aug31a/sketch_aug31a.ino
@@ -7,22 +7,81 @@
 
 RTC_DS1307 rtc;
 Adafruit_SSD1306 display(-1);
+int bPin7 = 7; 
+int botaodisplay =0;
 char Time[7];
 char daysOfTheWeek[7][12] = {"Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"};
-String horas;
-String minutos;
-String segundos;
-String hora;
 void setup () {
-  Serial.begin(57600);
+  Serial.begin(9600);
   display.begin(SSD1306_SWITCHCAPVCC, 0x3C); 
+  pinMode(bPin7, INPUT); 
   display.clearDisplay();
+  inicializartc();
+}
+ 
+void loop () {
+ ligadesliga(bPin7);
+ /*
+ display.ssd1306_command(SSD1306_DISPLAYOFF);//desliga display
+ botaodisplay = digitalRead(bPin7);
+ 
+ if (botaodisplay == HIGH){
+  display.ssd1306_command(SSD1306_DISPLAYON);
+  }
+*/
+    
+    DateTime now = rtc.now();
+    Serial.print(now.year(), DEC);
+    Serial.print('/');
+    Serial.print(now.month(), DEC);
+    Serial.print('/');
+    Serial.print(now.day(), DEC);
+    Serial.print('/');
+    Serial.print(now.hour(), DEC);
+    Serial.print(':');
+    Serial.print(now.minute(), DEC);
+    Serial.print(':');
+    Serial.print(now.second(), DEC);
+    Serial.println();
 
-#ifndef ESP8266
-  while (!Serial); // wait for serial port to connect. Needed for native USB
-#endif
+    // calculate a date which is 7 days, 12 hours, 30 minutes, and 6 seconds into the future
+    DateTime future (now + TimeSpan(7,12,30,6));
 
-  if (! rtc.begin()) {
+
+
+mostrahoradata((int)now.hour(),(int)now.minute(),(int)now.second(),(int)now.year(),(int)now.month(),(int)now.day());
+
+delay(2000);
+
+
+}
+
+int mostrahoradata(int horas,int minutos,int segundos,int dia,int mes, int ano){
+delay(200);
+display.setTextSize(1);
+display.setTextColor(WHITE);
+display.setCursor(70,10);
+display.print(horas, DEC) ;
+display.print(":");
+display.print(minutos,DEC);
+display.print(":");
+display.print(segundos,DEC);
+display.setCursor(0,10);
+display.print(ano, DEC) ;
+display.print("/");
+display.print(mes,DEC);
+display.print("/");
+display.print(dia,DEC);
+display.display();//changes have effect
+
+delay(1000);
+display.clearDisplay();
+return 0;
+
+}
+
+void inicializartc(){
+   if (! rtc.begin()) {
     Serial.println("Couldn't find RTC");
     Serial.flush();
     abort();
@@ -32,78 +91,24 @@ void setup () {
     Serial.println("RTC is NOT running, let's set the time!");
     // When time needs to be set on a new device, or after a power loss, the
     // following line sets the RTC to the date & time this sketch was compiled
-    rtc.adjust(DateTime(F(__DATE__), F(__TIME__)));
-    // This line sets the RTC with an explicit date & time, for example to set
+   // rtc.adjust(DateTime(F(__DATE__), F(__TIME__)));
     // January 21, 2014 at 3am you would call:
     // rtc.adjust(DateTime(2014, 1, 21, 3, 0, 0));
   }
-
-  // When time needs to be re-set on a previously configured device, the
-  // following line sets the RTC to the date & time this sketch was compiled
-  // rtc.adjust(DateTime(F(__DATE__), F(__TIME__)));
-  // This line sets the RTC with an explicit date & time, for example to set
-  // January 21, 2014 at 3am you would call:
-  // rtc.adjust(DateTime(2014, 1, 21, 3, 0, 0));
 }
 
-void loop () {
-    DateTime now = rtc.now();
-
-    Serial.print(now.year(), DEC);
-    Serial.print('/');
-    Serial.print(now.month(), DEC);
-    Serial.print('/');
-    Serial.print(now.day(), DEC);
-    Serial.print(" (");
-    Serial.print(daysOfTheWeek[now.dayOfTheWeek()]);
-    Serial.print(") ");
-    Serial.print(now.hour(), DEC);
-    Serial.print(':');
-    Serial.print(now.minute(), DEC);
-    Serial.print(':');
-    Serial.print(now.second(), DEC);
-    Serial.println();
-
-    Serial.print(" since midnight 1/1/1970 = ");
-    Serial.print(now.unixtime());
-    Serial.print("s = ");
-    Serial.print(now.unixtime() / 86400L);
-    Serial.println("d");
-
-    // calculate a date which is 7 days, 12 hours, 30 minutes, and 6 seconds into the future
-    DateTime future (now + TimeSpan(7,12,30,6));
-
-    Serial.print(" now + 7d + 12h + 30m + 6s: ");
-    Serial.print(future.year(), DEC);
-    Serial.print('/');
-    Serial.print(future.month(), DEC);
-    Serial.print('/');
-    Serial.print(future.day(), DEC);
-    Serial.print(' ');
-    Serial.print(future.hour(), DEC);
-    Serial.print(':');
-    Serial.print(future.minute(), DEC);
-    Serial.print(':');
-    Serial.print(future.second(), DEC);
-    Serial.println();
+int ligadesliga(int pino){
+/* para controlar a liga e desliga usar 2 pinos e fechar um um cnector */
+  
+  display.ssd1306_command(SSD1306_DISPLAYON);//desliga display 
+  botaodisplay = digitalRead(bPin7);
+ if (botaodisplay == HIGH){
+  display.ssd1306_command(SSD1306_DISPLAYOFF);
+  }
+  return 0;
+  
+  }
 
 
-//String horas = 
-String minutos = String((int)now.minute());  
-String segundos = String((int)now.second());
-String hora = horas+':';
-Serial.println();
-delay(200);
-display.setTextSize(1);
-display.setTextColor(WHITE);
-display.setCursor(0,10);
-display.print((int)now.hour(), DEC) ;
-display.print(":");
-display.print((int)now.minute(),DEC);
-display.print(":");
-display.print((int)now.second(),DEC);
-
-  display.display();
-  delay(1000);
-  display.clearDisplay();
-}
+  
+  
