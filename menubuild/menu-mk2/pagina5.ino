30b0abde765445c322155644fb6d410635e8172b
Mr-76 <thegreath76@gmail.com>
2022-03-08 11:37:55 -0300

    first commit

--- menubuild/menu-mk2/pagina5.ino
+++ menubuild/menu-mk2/pagina5.ino
@@ -1,14 +1,17 @@
 void pagina_5(){
-  display.setTextSize(1); 
-  display.setTextColor(1, 0);
-  display.setCursor(0,0);
-  display.println("VOLTAGEM BATERIA");
- 
-
-  display.setTextColor(1, 0); 
-  display.setCursor(0,20);
-  display.println("VOLTAGEM PROBES");
-  display.display();
-  delay(500); 
+  while(true){
+    int sensorValue = analogRead(A0);
+    float voltage= sensorValue * (5.0 / 1023.0);
+    display.setTextSize(1); 
+    display.setTextColor(1, 0);
+    display.setCursor(0,0);
+    display.println("VOLTAGEM PROBE");
+  
+    display.setTextColor(1,0); 
+    display.setCursor(0,20);
+    display.print(voltage, DEC);
+    display.display();
+  delay(500);
+ }
 }
 
