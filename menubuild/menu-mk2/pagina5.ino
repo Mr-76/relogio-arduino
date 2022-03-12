e4178b2bd09775e7b00bd191a71af1eb41883f9e
Mr-76 <thegreath76@gmail.com>
2022-03-12 08:47:20 -0300

    voltagem pra merge

--- menubuild/menu-mk2/pagina5.ino
+++ menubuild/menu-mk2/pagina5.ino
@@ -1,17 +1,18 @@
 void pagina_5(){
-  while(true){
-    int sensorValue = analogRead(A0);
-    float voltage= sensorValue * (5.0 / 1023.0);
-    display.setTextSize(1); 
-    display.setTextColor(1, 0);
-    display.setCursor(0,0);
-    display.println("VOLTAGEM PROBE");
   
-    display.setTextColor(1,0); 
-    display.setCursor(0,20);
-    display.print(voltage, DEC);
-    display.display();
-  delay(500);
- }
+	int sensorValue = analogRead(A0);
+	while(true){
+		float voltage= sensorValue * (5.0 / 1023.0);
+		display.setTextSize(1); 
+		display.setTextColor(1, 0);
+		display.setCursor(0,0);
+		display.println("VOLTAGEM PROBE");
+
+		display.setTextColor(1,0); 
+		display.setCursor(0,20);
+		display.print(voltage, DEC);
+		display.display();
+		delay(500);
+	}
 }
 
