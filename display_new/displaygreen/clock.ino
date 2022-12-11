790b94603e36b4c7c7b50d9dd25ca14b07d60386
Mr.76 <bartmoss77@tutanota.com>
2022-12-11 14:55:53 -0300

    func alarme pseudo codigo

--- display_new/displaygreen/clock.ino
+++ display_new/displaygreen/clock.ino
@@ -1,19 +1,74 @@
-void clock_menu(){
-menu_selector = 0;
- get_buttons_states();
- decrease_increase_menu();
- lcd.clear();
- lcd.setCursor(0,0);
- lcd.print("clock menu");
-  while (button2State == LOW){
+void clock_menu() {
+  menu_selector = 0;
+  get_buttons_states();
+  decrease_increase_menu();
+  lcd.clear();
+  lcd.setCursor(0, 0);
+  lcd.print("clock menu");
+  delay(500);
+  lcd.clear();
+  
+
+  while (true) {
     get_buttons_states();
     decrease_increase_menu();
     lcd.setCursor(0, 0);
     lcd.print(clock_menus[menu_selector]);
-
+    if (button2State == HIGH) {
+      //aposs selecao nao esta funcionando!! obs
+         
+      get_buttons_states();
+      decrease_increase_menu();
+      lcd.setCursor(0, 0);
+      lcd.print("TEST");
+      delay(1000);
+      //apos isso fica tudo sem nd no display:()
+      if (menu_selector == 0) {
+        lcd.clear();
+        lcd.setCursor(0, 0);
+        lcd.print("entrando");
+        get_buttons_states();
+        alarmMennu();
+      }
+    }
+    if (button3State == HIGH && button1State == HIGH){
+      break;
+    }
   }
+
   lcd.clear();
- lcd.setCursor(0,0);
- lcd.print("exiting clock");
- delay(1000);
+  lcd.setCursor(0, 0);
+  lcd.print("exiting clock");
+  delay(1000);
+}
+
+
+
+
+void alarmMennu() {
+  menu_selector = 0;
+  int alarm_timing = 1000;  //hora atual em segundos
+  get_buttons_states();
+  decrease_increase_menu();
+  lcd.clear();
+  lcd.setCursor(0, 0);
+  lcd.print("ALARMEEEE");
+  while (true) {
+    lcd.clear();
+    delay(1000);
+    //calculando horas
+    alarm_timing = 1000;  //hora atual em segundos
+    int hora = alarm_timing / 360;
+    int minutos = (alarm_timing - hora) * 360;
+    int segundos = (hora + minutos) - alarm_timing;
+
+    lcd.setCursor(0, 0);
+    lcd.print("STUFF");
+    //lcd.setCursor(3, 0);
+    //lcd.print(minutos);
+    //lcd.setCursor(5, 0);
+    //lcd.print(segundos);
+
+    decrease_increase_menu();
+  }
 }
--- display_new/displaygreen/displaygreen.ino
+++ display_new/displaygreen/displaygreen.ino
@@ -1,11 +1,11 @@
 #include <LiquidCrystal_I2C.h>
 //future change each menu to an object....
 //need to change the menu cases instead of if to switch and case.
-LiquidCrystal_I2C lcd(0x27,16,2);  // set the LCD address to 0x3F for a 16 chars and 2 line display
+LiquidCrystal_I2C lcd(0x27, 16, 2);  // set the LCD address to 0x3F for a 16 chars and 2 line display
 
 const int BUTTON1 = 8;
-const int BUTTON2 = 7;  
-const int BUTTON3 = 6;  
+const int BUTTON2 = 7;
+const int BUTTON3 = 6;
 
 int button1State = 0;
 int button2State = 0;
@@ -16,11 +16,11 @@ int menu_selector = 0;
 int menu_array[10];
 
 char *array_menus[] = {
-  "CLOCK", "RADIO FREQ", "INFRA RED","nothing"
+  "CLOCK", "RADIO FREQ", "INFRA RED", "nothing"
 };
 
 char *clock_menus[] = {
-  "ALARM", "COUNTER", "CRONOMETER","nothing"
+  "ALARM", "COUNTER", "CRONOMETER", "nothing"
 };
 
 
@@ -31,108 +31,113 @@ void setup() {
   pinMode(BUTTON2, INPUT);
   pinMode(BUTTON3, INPUT);
   lcd.init();
-  lcd.clear();         
+  lcd.clear();
   lcd.backlight();
 }
 
 //loop where the program starts
 void loop() {
-  get_buttons_states();//receive buttons states
-  decrease_increase_menu(); //testing if buttons are being pressed
- 
-  selection_menu();//menu selection
+  get_buttons_states();      //receive buttons states
+  decrease_increase_menu();  //testing if buttons are being pressed
+
+  selection_menu();  //menu selection
   show_options();
 }
 
 //shows the menus options to display
-void  show_options(){
-       lcd.setCursor(0,0);
-       lcd.clear(); 
-       lcd.print(array_menus[menu_selector]);
-       lcd.setCursor(2,1);//x,y
-       lcd.print(menu_selector);
-       delay(1000);
+void show_options() {
+  lcd.setCursor(0, 0);
+  lcd.clear();
+  lcd.print(array_menus[menu_selector]);
+  lcd.setCursor(2, 1);  //x,y
+  lcd.print(menu_selector);
+  delay(1000);
 }
 
 //read all buttons states
-void get_buttons_states(){
+void get_buttons_states() {
   button1State = digitalRead(BUTTON1);
   button2State = digitalRead(BUTTON2);
   button3State = digitalRead(BUTTON3);
 }
 
 //menu of selections,change to swich and case style..
-void selection_menu(){
+void selection_menu() {
   get_buttons_states();
-  if (button2State == HIGH){
+  if (button2State == HIGH) {
     if (menu_selector == 0) {
-         lcd.setCursor(0,0);
-         lcd.clear();  
-         lcd.print("menu_selecionado");
-         delay(1000);
-         clock_menu();
-  }else if (menu_selector == 1){
-         lcd.setCursor(0,0);
-         lcd.clear();  
-         lcd.print("menu_selecionado");
-         delay(1000);
-         radio_freq_menu();
-    
-  }else if (menu_selector == 2){
-          lcd.setCursor(0,0);
-          lcd.clear();  
-         lcd.print("menu_selecionado");
-         delay(1000);
+      lcd.setCursor(0, 0);
+      lcd.clear();
+      lcd.print("menu_selecionado");
+      delay(1000);
+      clock_menu();
+    } else if (menu_selector == 1) {
+      lcd.setCursor(0, 0);
+      lcd.clear();
+      lcd.print("menu_selecionado");
+      delay(1000);
+      radio_freq_menu();
 
-    infra_red_menu();
-    
-  }else{
-    ;
-  }
+    } else if (menu_selector == 2) {
+      lcd.setCursor(0, 0);
+      lcd.clear();
+      lcd.print("menu_selecionado");
+      delay(1000);
+
+      infra_red_menu();
+
+    } else {
+      ;
+    }
   }
 }
 
 //change the menu control variable
-void decrease_increase_menu(){
+void decrease_increase_menu() {
   if (button1State == HIGH) {
-       lcd.setCursor(0,0);
-       lcd.clear(); 
-       lcd.print("buttom1 pressed");
-       delay(1000);
-    
-       menu_selector--;
-    	
-       limit_menu();
+    lcd.setCursor(0, 0);
+    lcd.clear();
+    lcd.print("buttom1 pressed");
+    delay(1000);
 
-   
-       lcd.setCursor(2,1);
-       lcd.print(menu_selector);
-       
-       
-  }else  if (button3State == HIGH) {
-       lcd.setCursor(0,0);
-       lcd.clear();  
-       lcd.print("buttom3 pressed");
-       delay(1000);
-       menu_selector++;
-      limit_menu();
-   
-    
-        lcd.setCursor(2,1);
-        lcd.print(menu_selector);
-  }
-  else{
+    menu_selector--;
+
+    limit_menu();
+
+
+    lcd.setCursor(2, 1);
+    lcd.print(menu_selector);
+    delay(500);
+
+    lcd.clear();
+
+
+  } else if (button3State == HIGH) {
+    lcd.setCursor(0, 0);
+    lcd.clear();
+    lcd.print("buttom3 pressed");
+    delay(1000);
+    menu_selector++;
+    limit_menu();
+
+
+    lcd.setCursor(2, 1);
+    lcd.print(menu_selector);
+    delay(500);
+    lcd.clear();
+
+  } else {
     ;
   }
 }
 
 //limiting the menu values. no max than the array size no min than 0
-void limit_menu(){
-  if (menu_selector <0){
+void limit_menu() {
+  if (menu_selector < 0) {
     menu_selector = 0;
-  }else if (menu_selector >= 4){
+  } else if (menu_selector >= 4) {
     menu_selector = 3;
-  }else{
+  } else {
     ;
   }
 }
@@ -141,37 +146,36 @@ void limit_menu(){
 
 
 //radio freq menu impl
-void radio_freq_menu(){
- get_buttons_states();
- decrease_increase_menu();
- lcd.clear();
- lcd.setCursor(0,0);
- lcd.print("freq menu");
-  while (button2State == LOW){
+void radio_freq_menu() {
+  get_buttons_states();
+  decrease_increase_menu();
+  lcd.clear();
+  lcd.setCursor(0, 0);
+  lcd.print("freq menu");
+  while (button2State == LOW) {
     get_buttons_states();
   }
   lcd.clear();
- lcd.setCursor(0,0);
- lcd.print("exiting freq");
- delay(1000);
+  lcd.setCursor(0, 0);
+  lcd.print("exiting freq");
+  delay(1000);
 }
 
 //infra red menu impl
-void infra_red_menu(){
-menu_selector = 0;
- get_buttons_states();
- decrease_increase_menu();
- lcd.clear();
- lcd.setCursor(0,0);
- lcd.print("infra red menu");
-  while (button2State == LOW){
+void infra_red_menu() {
+  menu_selector = 0;
+  get_buttons_states();
+  decrease_increase_menu();
+  lcd.clear();
+  lcd.setCursor(0, 0);
+  lcd.print("infra red menu");
+  while (button2State == LOW) {
     get_buttons_states();
-    
-    decrease_increase_menu();
 
+    decrease_increase_menu();
   }
   lcd.clear();
- lcd.setCursor(0,0);
- lcd.print("exiting infra");
- delay(1000);
+  lcd.setCursor(0, 0);
+  lcd.print("exiting infra");
+  delay(1000);
 }
